b7c79c8fa70ae92edf71e3a56fea77ba07e683e7
Won-Doo Seo <wdseo@yonsei.ac.kr>
2024-06-20 11:43:30 +0900

    Update EIT_Run.m

--- EIT_Run.m
+++ EIT_Run.m
@@ -6,21 +6,17 @@ clc; clearvars; close all;
 
 EIT_EIDORS_Model_FER();
 EIT_EIDORS_Model_GREIT();
-EIT_LCT_Normal1_FER();
-EIT_LCT_Normal1_GREIT();
-EIT_LCT_Normal2_FER();
-EIT_LCT_Normal2_GREIT();
-EIT_LCT_Normal3_FER();
-EIT_LCT_Normal3_GREIT();
-EIT_LCT_Normal4_FER();
-EIT_LCT_Normal4_GREIT();
-EIT_LCT_Normal5_FER();
-EIT_LCT_Normal5_GREIT();
-EIT_LCT_Normal6_FER();
-EIT_LCT_Normal6_GREIT();
-EIT_LCT_Obese1_FER();
-EIT_LCT_Obese1_GREIT();
+
+for i=1:6
+    run(strcat('./Normal/EIT_LCT_Normal', int2str(i), '_FER.m')
+    run(strcat('./Normal/EIT_LCT_Normal', int2str(i), '_GREIT.m')
+end
+
+for i=1:3
+    run(strcat('./Normal/EIT_LCT_Obese', int2str(i), '_FER.m')
+    run(strcat('./Normal/EIT_LCT_Obese', int2str(i), '_GREIT.m')
+end
+
 % EIT_LCT_Obese2_FER();     % No use
 % EIT_LCT_Obese2_GREIT();   % No use
-EIT_LCT_Obese3_FER();
-EIT_LCT_Obese3_GREIT();
+% I have no idea this shorten code will work
